d0aeeb9441ec9276a3d1d9975dba3fc7cad3ef88
Jari Bakken <jari.bakken@gmail.com>
2013-03-05 00:29:16 +0100

    Bump JRuby to 1.7.2.
    This means we're now running JRuby compatible with Ruby 1.9.
    
    Also:
    
      * Simplify go and go.bat by pulling common stuff into rake-tasks/main.rb
      * Remove json_pure.jar and rake.jar, since json and rake are part of the JRuby jar.

--- go.bat
+++ go.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -Xmx2048m -XX:MaxPermSize=1024m -XX:ReservedCodeCacheSize=256m -client -jar third_party\jruby\jruby-complete.jar -r third_party\jruby\gems\albacore.jar -r third_party\jruby\gems\rake.jar -r third_party\jruby\gems\childprocess.jar -X-C -S rake %*
+java -Xmx2048m -XX:MaxPermSize=1024m -XX:ReservedCodeCacheSize=256m -client -jar third_party\jruby\jruby-complete.jar -X-C .\rake-tasks\main.rb %*
---
+++ rake-tasks/main.rb
@@ -0,0 +1,5 @@
+require File.expand_path("../../third_party/jruby/gems/albacore.jar", __FILE__)
+require File.expand_path("../../third_party/jruby/gems/childprocess.jar", __FILE__)
+require 'rake'
+
+Rake.application.run
